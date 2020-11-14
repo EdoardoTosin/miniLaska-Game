3bd480ef38eba292c979cd875c82a599a230de39
Edoardo Tosin <iEdo@live.it>
2020-11-14 15:02:36 +0100

    Add files via upload

---
+++ miniLaska.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Square{
+	int height;
+	int pos_1;
+	int pos_2;
+	int pos_3;
+};
+
+int square_status(){ /*Controllo casella*/
+	return 0;
+}
+
+int eat(){ /*Conquista*/
+	return 0;
+}
+
+int win_check(){ /*Controllo condizioni vittoria dopo ogni mossa*/
+	return 0;
+}
+
+int table_out(int pos){ /*Creazione grafica output*/
+	return 0;
+}
+
+int main(){
+
+	return 0;
+}
