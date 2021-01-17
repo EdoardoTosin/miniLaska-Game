dc5130b127d89f07cdc33a21d75dc0dee825eb85
Edoardo Tosin <edoardotosin@mail.com>
2021-01-17 18:35:36 +0100

    Added script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -o errexit
+
+rm -rf build
+mkdir build
+cd build
+
+#Generate build folder
+cmake ../.
+#make, execute compile command
+make
+#run
+./miniLaska
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -o errexit
+
+#run
+./miniLaska
+set +o errexit
